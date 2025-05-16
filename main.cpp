7e804d3ca4fc171cbbf73efacaeb64b05795018b
Andrey-vkr <147654322+Andrey-vkr@users.noreply.github.com>
2025-05-16 12:28:18 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Chat.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	Chat chat;
+	chat.startChat();
+	while (chat.isChatRunning())
+	{
+		chat.showRegistration();
+
+		while (chat.getCurrentUser())
+		{
+			chat.showMessages();
+		}
+	}
+
+	return 0;
+}
