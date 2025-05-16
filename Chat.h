416eaa1402b1ff20cc16cb940a3e8a36ac4711fc
Andrey-vkr <147654322+Andrey-vkr@users.noreply.github.com>
2025-05-16 12:29:55 +0300

    4

---
+++ Chat.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <vector>
+#include <memory>
+#include "Message.h"
+
+
+class Chat
+{
+private:
+	bool running = false;
+	std::vector<User> users;
+	std::vector<Message> messages;
+	std::shared_ptr<User> currentUser;
+	void login();
+	void signUp();
+	void showChat() const;
+	void showAllUsers() const;
+	void addMessage();
+
+	std::vector<User>& getUsers()
+	{
+		return users;
+	}
+
+	std::vector<Message>& getMessages()
+	{
+		return messages;
+	}
+
+	std::shared_ptr<User> findUserByLogin(const std::string& login) const;
+	std::shared_ptr<User> findUserByName(const std::string& name) const;
+
+public:
+	void startChat();
+
+	bool isChatRunning() const
+	{
+		return running;
+	}
+
+	std::shared_ptr<User> getCurrentUser() const
+	{
+		return currentUser;
+	}
+
+	void showRegistration();
+	void showMessages();
+};
+
